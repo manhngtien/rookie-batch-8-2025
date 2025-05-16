e05542b939eb2f578358b9e20ca1a771d53ddbb8
LuanTang <luantang.work@gmail.com>
2025-05-16 17:24:02 +0700

    feat: add core interfaces and entities

---
+++ AssetManagement/AssetManagement.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using AssetManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+     
+    }
+}
