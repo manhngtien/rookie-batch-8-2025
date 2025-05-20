c0b7a15d0c8f67ec2ea0bd0afd5f7e2a439047e5
LuanTang <luantang.work@gmail.com>
2025-05-20 18:30:00 +0700

    fix: password now no longer need to contain at least 1 upper case letter

--- AssetManagement/AssetManagement.Application/Validators/Accounts/ChangePasswordRequestValidator.cs
+++ AssetManagement/AssetManagement.Application/Validators/Accounts/ChangePasswordRequestValidator.cs
@@ -12,7 +12,6 @@ namespace AssetManagement.Application.Validators.Accounts
             RuleFor(x => x.NewPassword)
                 .NotEmpty().WithMessage("New password is required.")
                 .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
-                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
                 .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
                 .Matches(@"[0-9]").WithMessage("Password must contain at least one number.")
                 .Matches(@"[\W]").WithMessage("Password must contain at least one special character.");
